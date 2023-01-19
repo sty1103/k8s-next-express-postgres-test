7b9f660899a356544a61c140299e7feb6ef00afa
Hyeonjun <sty110357@gmail.com>
2023-01-19 19:09:39 +0900

    [ASDF

---
+++ server/index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+const multer = require('multer');
+const fs = require('fs');
+const port = 8080
+
+// CORS 설정
+// const cors = require('cors');
+// const whitelist = ['http://localhost:3000'];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('not allowed origin'));
+//     }
+//   }
+// }
+// app.use(cors(corsOptions));
+
+app.post('/api', (req, res) => {
+  res.status(200).send({hello:'world'});
+});
+
+//////////////// 파일 업로드 //////////////////////
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const path = '/';
+    cb(null, path)
+
+    // 디렉터리 없으면 디렉터리를 새로 생성
+    if ( !fs.existsSync(path) ) 
+      fs.mkdirSync(path, { recursive: true })
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+const upload = multer({ storage })
+
+app.post('/api/upload', upload.single('img'), (req, res) => {
+  res.status(200).send({
+    message: 'ok',
+    fileInfo: req.file
+  })
+})
+
+//////////////// 디비 테스트 //////////////////////
+const { Client } = require('pg')
+const client = new Client({
+  user: "test",
+  host: "localhost",
+  database: "test",
+  password: "test",
+  port: 5432
+});
+
+client.connect()
+
+app.post('/api/db', (req, res) => {
+  client.query(`INSERT INTO customer(password) VALUES('1234')`).then((res) => {
+    console.log( res );
+    client.end()
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
